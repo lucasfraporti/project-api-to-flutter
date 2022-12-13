0a142f35af307cb6435bddb02a8ec5635e8b04d4
Lucas Fraporti <80408351+lucasfraporti@users.noreply.github.com>
2022-12-13 20:29:30 -0300

    Update App.js

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ dotenv.config();
 App.use(express.json());
 App.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.CLUSTER}/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://larissabernardon:passwordpassword123@cluster0.maizbww.mongodb.net/?retryWrites=true&w=majority`)
 .then(() => {
     console.log('Conectado ao MongoDB com sucesso');
 }).catch((error) => {
